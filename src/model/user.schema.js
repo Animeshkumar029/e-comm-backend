a75c572ba8b7915181a653f357514bf4a1a6f9e2
Animeshkumar029 <128279338+Animeshkumar029@users.noreply.github.com>
2025-03-04 11:58:39 +0000

    schema design

---
+++ src/model/user.schema.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import AuthRoles from "../utils/AuthRoles.js";
+
+const userSchema=new mongoose.Schema(
+    {
+        name:{
+            type: String,
+            required:[true,"provide a name for the user"],
+            trim: true,
+            maxLength:[50,"name must be less than or equal to 50 characters"]
+        },
+        email:{
+            type:String,
+            required:[true,"Give your email"]
+        },
+        roles:{
+            type:String,
+            enum: Object.values(AuthRoles),
+            default: AuthRoles.USER
+        },
+        password:{
+            type: String,
+            required:[true,"password must be provided"],
+            select:false,
+            minLength:[8,"password must be of atleast 8 characters"]
+        },
+        forgotPasswordToken: String,
+        forgotPasswordExpiry: Date
+    },
+    {timeStamps: true}
+);
+
+export default mongoose.model("User",userSchema);
