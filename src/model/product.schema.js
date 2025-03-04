58c904c8ba04101526a3f94327265cd12aae3422
Animeshkumar029 <128279338+Animeshkumar029@users.noreply.github.com>
2025-03-04 13:23:49 +0000

    product schema cerated

---
+++ src/model/product.schema.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const productSchema=new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:[true,"provide a name"],
+            trim:true,
+            maxLength:[120,"product name should not be more than of 120 characters"]
+        },
+        price:{
+            type:Number,
+            required:[true,"provide price of the product"],
+            maxLength:[5,"product price should not be more than 5 chars"]
+        },
+        description:{
+            type:String
+        },
+        photos:[
+            {
+                secure_url:{
+                    type:String,
+                    required:true
+                }
+            }
+        ],
+        stock:{
+            type:Number,
+            default:0
+        },
+        sold:{
+            type:Number,
+            default:0
+        },
+        collectionId:{
+            ref: "Collection"
+        }
+    },
+    {timestamps:true}
+)
+
+export default mongoose.model("Product",productSchema);
